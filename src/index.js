89ebf882fe57c18ec1d7c8d22d09c8b5a3c5f93e
MichaelDelcambre <delcambrem@gmail.com>
2021-12-14 00:43:27 -0600

    commit tree 9ace5d4ec98d9b0d0ea1de658c47698409369ce3

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+// import Article from "./components/Article"
+// import ArticleList from "./components/ArticleList"
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
